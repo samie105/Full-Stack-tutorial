a722a3a60322cf04069b59071179012dbb08bc8a
unknown <samsonrichfield@gmail.com>
2023-04-22 09:14:17 +0100

    corrected cors file

--- backend/app.js
+++ backend/app.js
@@ -7,15 +7,6 @@ const userRoute = require("./routes/userRoute");
 
 const app = express();
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 const corsOptions = {
   origin: "https://todoapperr.netlify.app",
 };
